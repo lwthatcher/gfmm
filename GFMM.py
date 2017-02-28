b704b986904c46fd9373c92306b2d0f61446a06a
Lawrence Thatcher <lwthatcher@msn.com>
2017-02-28 13:27:26 -0700

    stubbed out GFMM class

---
+++ GFMM.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class GFMM:
+
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def predict(self):
+        pass
+
+    def _expansion(self):
+        pass
+
+    def _overlap_test(self):
+        pass
+
+    def _contraction(self):
+        pass
