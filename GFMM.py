3827e13c7096a6b97a6f4b3a4c8ee98af508b8d0
Lawrence Thatcher <lwthatcher@msn.com>
2017-02-28 17:44:28 -0700

    starting to initialize data

--- GFMM.py
+++ GFMM.py
@@ -6,7 +6,9 @@ class GFMM:
 
     def __init__(self, membership_func=membership.General):
         print('Hello GFMM')
-        pass
+        self.X_l = np.zeros((0, 0))
+        self.X_u = np.zeros((0, 0))
+        self.n = 0
 
     def fit(self, X, Y):
         """
@@ -16,7 +18,12 @@ class GFMM:
             Target Values
             note that d=0 corresponds to an unlabeled item
         """
-        pass
+        # TODO: check to see if X is (X_l, X_u); otherwise default to assume they are the same
+        self.X_l = X
+        self.X_u = np.copy(X)
+
+        self.n = len(X[0])
+        self._initialize(self.n)
 
     def predict(self, X):
         pass
