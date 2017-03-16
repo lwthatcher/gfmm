acfb2efd862491e9ebd41fce1a9b96789d37ad36
Lawrence Thatcher <lwthatcher@msn.com>
2017-03-16 16:48:47 -0600

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup for GFMM"""
+
+from distutils.core import setup
+
+setup(
+    name='gfmm',
+    version='0.1.0',
+    description='General Fuzzy Min-Max neural network',
+    author='Lawrence Thatcher',
+    author_email='lwthatcher@msn.com',
+    url='https://github.com/lwthatcher/gfmm',
+    packages=['gfmm']
+)
