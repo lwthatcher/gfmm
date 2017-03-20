e45d92b0424532148ff5a90f53cca460b99adf00
Lawrence Thatcher <lwthatcher@msn.com>
2017-03-20 15:51:26 -0600

    cleaned up iris.py

--- examples/iris.py
+++ examples/iris.py
@@ -20,31 +20,8 @@ def run_iris():
     target.astype(int)
 
     print("shuffling data, and dividing into Train/Test sets")
-    kwargs = {"p":3, "theta":0.1}
+    kwargs = {"p":3}
     cross_fold(data, target, 10, **kwargs)
-
-    # idx = np.arange(len(target))
-    # np.random.shuffle(idx)
-    # idx_train = idx[:-20]
-    # idx_test = idx[-20:]
-    # X_train = data[idx_train,:]
-    # Y_train = target[idx_train]
-    # X_test = data[idx_test,:]
-    # Y_test = target[idx_test]
-    # # X = data[idx,:]
-    # # Y = target[idx]
-    # # print("splitting into folds")
-    # # Xs = np.split(X, 10)
-    # # Ys = np.split(Y, 10)
-    #
-    # print("creating classifier")
-    # gfmm = GFMM(p=3)
-    # gfmm.fit(X_train, Y_train)
-    # out = gfmm.predict(X_test)
-    # print("Actual")
-    # print(Y_test)
-    # print("Predicted")
-    # print(out)
 
 
 if __name__ == "__main__":
